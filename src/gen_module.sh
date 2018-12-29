4571ac41bc2199b5e6ef4093a7e94982e4421241
nutti <nutti.metro@gmail.com>
2018-12-29 16:05:15 +0900

    Fix: usage of gen_module.sh option

--- src/gen_module.sh
+++ src/gen_module.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
-TMP_DIR=./gen_module-tmp
+TMP_DIR_NAME=gen_module-tmp
 
 if [ $# -ne 4 ]; then
-    echo "Usage: sh gen_module.sh <source-dir> <blender-dir> <version> <output-dir>"
+    echo "Usage: sh gen_module.sh <source-dir> <blender-dir> <branch/tag/commit> <output-dir>"
     exit 1
 fi
 
 source_dir=${1}
 blender_dir=${2}
-version=${3}
+branch_name=${3}
 output_dir=${4}
 current_dir=`pwd`
+tmp_dir=${current_dir}/${TMP_DIR_NAME}
 
 # make temporary directory
-mkdir -p ${TMP_DIR}
+mkdir -p ${tmp_dir}
 
-# change to the target branch/tag
+# change to the target branch/tag/commit
 cd ${source_dir}
 git fetch --prune
-git checkout ${version}
-git pull origin ${version}
-
-cd ${current_dir}
+git checkout ${branch_name}
+git pull origin ${branch_name}
 
 # generate .rst documents
-${blender_dir}/blender --background --factory-startup -noaudio --python ${source_dir}/doc/python_api/sphinx_doc_gen.py -- --output ${TMP_DIR}
+cd ${current_dir}
+${blender_dir}/blender --background --factory-startup -noaudio --python ${source_dir}/doc/python_api/sphinx_doc_gen.py -- --output ${tmp_dir}
 
 # convert .rst to .xml
-mkdir -p ${TMP_DIR}/sphinx-out-xml
-sphinx-build -b xml ${TMP_DIR}/sphinx-in ${TMP_DIR}/sphinx-out-xml
+mkdir -p ${tmp_dir}/sphinx-out-xml
+sphinx-build -b xml ${tmp_dir}/sphinx-in ${tmp_dir}/sphinx-out-xml
 
 # generate fake bpy modules
-python gen.py -i ${TMP_DIR}/sphinx-out-xml -o ${output_dir} -t pycharm -f pep8
+python gen.py -i ${tmp_dir}/sphinx-out-xml -o ${output_dir} -t pycharm -f pep8
 
 # clear temporary directory
 cd ${current_dir}
-rm -rf ${TMP_DIR}
+rm -rf ${tmp_dir}
