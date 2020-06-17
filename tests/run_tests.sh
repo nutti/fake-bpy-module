2583e5c96a166df64107a62e7955eadc26b8e9c3
Christian S <2787581+grische@users.noreply.github.com>
2020-06-17 07:38:33 +0200

    Add: Check python version checks in bash scripts (#33)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,6 +8,21 @@ fi
 PACKAGES_PATH=${1}
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 TMP_DIR_NAME="tmp"
+PYTHON_BIN=${PYTHON_BIN:-python3}
+
+# check if PYTHON_BIN binary is availble
+if ! command -v ${PYTHON_BIN} > /dev/null; then
+    echo "Error: Cannot find ${PYTHON_BIN} binary."
+    exit 1
+fi
+python_bin=$(command -v ${PYTHON_BIN})
+
+# check if python version meets our requirements
+IFS=" " read -r -a python_version <<< "$(${python_bin} -c 'import sys; print(sys.version_info[:])' | tr -d '(),')"
+if [ ${python_version[0]} -lt 3 ] || [[ "${python_version[0]}" -eq 3 && "${python_version[1]}" -lt 6 ]]; then
+    echo "Error: Unsupported python version \"${python_version[0]}.${python_version[1]}\". Requiring python 3.6 or higher."
+    exit 1
+fi
 
 tmp_dir_path="${SCRIPT_DIR}/${TMP_DIR_NAME}"
 
@@ -24,7 +39,7 @@ for pkg in ${package_list}; do
     mkdir ${tmp_dir_path}
     unzip ${pkg} -d ${tmp_dir_path}
     pkg_dir_path=${tmp_dir_path}/${pkg_dir_name}
-    python ${SCRIPT_DIR}/python/run_tests.py -p ${pkg_dir_path} -v ${blender_version}
+    ${python_bin} ${SCRIPT_DIR}/python/run_tests.py -p ${pkg_dir_path} -v ${blender_version}
     if [ $? -ne 0 ]; then
         echo "Test Failure. (${pkg})"
         exit 1
