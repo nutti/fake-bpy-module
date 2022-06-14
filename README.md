59a6ca6859e7b6c0605dd9bec6c2b05342d06ae3
nutti <nutti.metro@gmail.com>
2022-06-14 18:46:05 +0900

    Add: Support for Blender v3.2

--- README.md
+++ README.md
@@ -58,6 +58,7 @@ pip install fake-bpy-module-latest
 |2.93|https://pypi.org/project/fake-bpy-module-2.93/|
 |3.0|https://pypi.org/project/fake-bpy-module-3.0/|
 |3.1|https://pypi.org/project/fake-bpy-module-3.1/|
+|3.2|https://pypi.org/project/fake-bpy-module-3.2/|
 |latest|https://pypi.org/project/fake-bpy-module-latest/|
 
 
--- src/gen.py
+++ src/gen.py
@@ -11,11 +11,11 @@ INPUT_DIR: str = "."
 SUPPORTED_TARGET: List[str] = ["pycharm"]
 SUPPORTED_STYLE_FORMAT: List[str] = ["none", "pep8"]
 SUPPORTED_MOD_BLENDER_VERSION: List[str] = [
-    "2.78", "2.79", "2.80", "2.81", "2.82", "2.83", "2.90", "2.91", "2.92", "2.93", "3.0", "3.1",
+    "2.78", "2.79", "2.80", "2.81", "2.82", "2.83", "2.90", "2.91", "2.92", "2.93", "3.0", "3.1", "3.2",
     "latest"
 ]
 SUPPORTED_BLENDER_VERSION: List[str] = [
-    "2.78", "2.79", "2.80", "2.81", "2.82", "2.83", "2.90", "2.91", "2.92", "2.93", "3.0", "3.1",
+    "2.78", "2.79", "2.80", "2.81", "2.82", "2.83", "2.90", "2.91", "2.92", "2.93", "3.0", "3.1", "3.2",
     "latest"
 ]
 MOD_FILES_DIR: str = os.path.dirname(os.path.abspath(__file__))
