89efbfef86f6c1f2fa16eff67420d76f1bb3a222
UuuNyaa <uuunyaa@gmail.com>
2023-12-31 15:01:08 -0800

    Unify code of get latest blender artifact url

--- tools/utils/download_latest_blender.sh
+++ tools/utils/download_latest_blender.sh
@@ -12,69 +12,17 @@ workflow=${3}
 target_dir=${4}
 token_for_actions=${5:-not-specified}
 
-echo "Trying to get URL candidates to get the artifact ..."
-artifacts_cands=$(
-    curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${owner}/${repository}/actions/runs" | \
-        jq ".workflow_runs[] |
-            select(.name == \"${workflow}\" and .conclusion == \"success\" and .artifacts_url != null)" | \
-        jq -s ''
-)
-
-artifacts_cands_count=$(
-    echo "${artifacts_cands}" | \
-        jq ". |
-            length"
-)
-if [ "${artifacts_cands_count}" -eq 0 ]; then
-    echo "  Candidates for artifacts are not found."
-    exit 1
-fi
-echo "  Found ${artifacts_cands_count} candidate(s)."
-
 echo "Trying to get the artifact URL ..."
-artifacts_url="null"
-for i in $(seq 1 "${artifacts_cands_count}"); do
-    index=$((i-1))
-    artifacts_api_url=$(
-        echo "${artifacts_cands}" | \
-            jq "sort_by(.created_at) |
-                reverse[${index}] |
-                .artifacts_url" | \
-            sed 's/^"\(.*\)"$/\1/'
-    )
-
-    artifacts=$(
-        curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "${artifacts_api_url}"
-    )
-
-    artifacts_count=$(
-        echo "${artifacts}" | \
-            jq ".artifacts | length"
-    )
-
-    if [ "${artifacts_count}" -ne 0 ]; then
-        artifacts_url=$(
-            curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "${artifacts_api_url}" | \
-                jq ".artifacts[0].archive_download_url" | \
-                sed 's/^"\(.*\)"$/\1/'
-        )
-        break
-    fi
-done
-
-if [ "${artifacts_url}" == "null" ]; then
-    echo "  Artifacts are not found."
-    exit 1
-fi
+artifacts_url=
 echo "  Artifact URL is ${artifacts_url}."
 
 echo "Downloading the artifact ..."
 mkdir -p "${target_dir}"
-if [ "${token_for_actions}" = "not-specified" ]; then
-    curl --location --fail -s "${artifacts_url}" -o artifact-blender.zip
-else
-    curl --location --fail -s -H "Authorization: token ${token_for_actions}" "${artifacts_url}" -o artifact-blender.zip
+curl_args="--location --fail -s"
+if [ "${token_for_actions}" != "not-specified" ]; then
+    curl_args+=" -H 'Authorization: token ${token_for_actions}'"
 fi
+curl ${curl_args} "${artifacts_url}" -o artifact-blender.zip
 
 echo "Decompressing the artifact ..."
 unzip -o -d "${target_dir}" artifact-blender.zip
--- tools/utils/get_latest_artifacts_url.sh
+++ tools/utils/get_latest_artifacts_url.sh
@@ -10,47 +10,21 @@ owner=${1}
 repository=${2}
 workflow=${3}
 
+artifacts_url="null"
 artifacts_cands=$(
     curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${owner}/${repository}/actions/runs" | \
-        jq ".workflow_runs[] |
-            select(.name == \"${workflow}\" and .conclusion == \"success\" and .artifacts_url != null)" | \
-        jq -s ''
+        jq -r ".workflow_runs[] |
+            select(.name == \"${workflow}\" and .conclusion == \"success\" and .artifacts_url != null) |
+            .artifacts_url"
 )
 
-artifacts_cands_count=$(
-    echo "${artifacts_cands}" | \
-        jq ". |
-            length"
-)
-if [ "${artifacts_cands_count}" -eq 0 ]; then
-    echo "Candidates for artifacts are not found."
-    exit 1
-fi
-
-artifacts_url="null"
-for i in $(seq 1 "${artifacts_cands_count}"); do
-    index=$((i-1))
-    artifacts_api_url=$(
-        echo "${artifacts_cands}" | \
-            jq -r "sort_by(.created_at) |
-                reverse[${index}] |
-                .artifacts_url"
+# artifacts_cands are sorted in created_at order when retrieved
+for artifacts_api_url in ${artifacts_cands}; do
+    artifacts_url=$(
+        curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "${artifacts_api_url}" | \
+            jq -r ".artifacts[0].archive_download_url"
     )
-
-    artifacts=$(
-        curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "${artifacts_api_url}"
-    )
-
-    artifacts_count=$(
-        echo "${artifacts}" | \
-            jq ".artifacts | length"
-    )
-
-    if [ "${artifacts_count}" -ne 0 ]; then
-        artifacts_url=$(
-            curl --location --fail -s -H "Accept: application/vnd.github.v3+json" "${artifacts_api_url}" | \
-                jq -r ".artifacts[0].archive_download_url"
-        )
+    if [ "${artifacts_url}" != "null" ]; then
         break
     fi
 done
