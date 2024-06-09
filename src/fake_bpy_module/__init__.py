d2be59876ddad4e7b24bf05330a075796a05dd76
nutti <nutti.metro@gmail.com>
2024-06-09 18:18:04 +0900

    WIP

--- src/fake_bpy_module/__init__.py
+++ src/fake_bpy_module/__init__.py
@@ -1,6 +1,9 @@
-from .analyzer.analyzer import analyze
 from .transformer.transformer import transform
 from .generator.generator import generate
 from . import config
 from .utils import check_os
 from . import support
+
+__all__ = [
+    "analyzer.analyzer.analyze"
+]
