c8afdaa004f685f8e31522e75906bb8f731a2305
Jonathan Plasse <jonathan.plasse@live.fr>
2024-08-14 21:09:30 +0200

    Bump minimal Python version to 3.12

--- tests/pylint_cycles.sh
+++ tests/pylint_cycles.sh
@@ -104,7 +104,7 @@ python_bin=$(command -v "${PYTHON_BIN}")
 # check if python version meets our requirements
 IFS=" " read -r -a python_version <<< "$(${python_bin} -c 'import sys; print(sys.version_info[:])' | tr -d '(),')"
 if [ "${python_version[0]}" -lt 3 ] || [[ "${python_version[0]}" -eq 3 && "${python_version[1]}" -lt 11 ]]; then
-    echo "Error: Unsupported Python version \"${python_version[0]}.${python_version[1]}\". Requiring Python 3.11 or higher."
+    echo "Error: Unsupported Python version \"${python_version[0]}.${python_version[1]}\". Requiring Python 3.12 or higher."
     exit 1
 fi
 
