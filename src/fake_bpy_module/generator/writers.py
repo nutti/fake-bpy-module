b58d4a5ec7810cb1b2a71bfb0f5c21ec02dfcbca
nutti <nutti.metro@gmail.com>
2025-03-30 23:06:35 +0530

    Fix: bl_ui_utils.layout is imported incorrectly

--- src/fake_bpy_module/generator/writers.py
+++ src/fake_bpy_module/generator/writers.py
@@ -618,11 +618,12 @@ class PyCodeWriterBase(BaseWriter):
             wt = self._writer
             wt.reset()
 
-            module_name = get_first_child(
-                    get_first_child(document, ModuleNode), NameNode).astext()
-            print(f"XXX {module_name}")
-            if module_name == "bl_ui_utils":
-                print(f"@@@ {document.pformat()}")
+            module_node = get_first_child(document, ModuleNode)
+            if module_node is not None:
+                module_name = get_first_child(module_node, NameNode).astext()
+                print(f"XXX {module_name}")
+                if module_name == "bl_ui_utils":
+                    print(f"@@@ {document.pformat()}")
 
             code_doc_nodes = find_children(document, CodeDocumentNode)
             doc_writer = CodeWriter()
