615577c547b7f2f61108fbe2fbed71e56e669487
nutti <nutti.metro@gmail.com>
2026-03-01 11:50:04 +0530

    WIP

--- src/fake_bpy_module/analyzer/readers.py
+++ src/fake_bpy_module/analyzer/readers.py
@@ -1,5 +1,5 @@
 from docutils.readers import standalone
-from docutils.transforms import references
+from docutils.transforms import references, frontmatter
 
 from fake_bpy_module.transformer.transformer_base import TransformerBase
 
@@ -8,5 +8,6 @@ class BpyRstDocsReader(standalone.Reader):
     def get_transforms(self) -> list[TransformerBase]:
         transforms = super().get_transforms()
         transforms.remove(references.DanglingReferences)
+        transforms.remove(frontmatter.DocTitle)
 
         return transforms
